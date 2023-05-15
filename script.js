a343c87b388cf790392cd330ebfab82c43c25544
Maks Cherevko <yourewen@gmail.com>
2023-05-15 11:39:22 +0300

    correction

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const menuButton = document.querySelector('.header__button');
 const menu = document.querySelector('.header__menu');
 
-menuButton.addEventListener('click', () => {
-    document.body.classList.toggle('lock');
-    menuButton.classList.toggle('active');
-    menu.classList.toggle('active');
-});
+window.onload = function() {
+
+    menuButton.addEventListener('click', () => {
+        document.body.classList.toggle('lock');
+        menuButton.classList.toggle('active');
+        menu.classList.toggle('active');
+    });
+    
+};
